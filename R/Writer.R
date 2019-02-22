95cee5043e63b8bcf050f2e98a7a5d34526ac938
Chloe H. Lee <hjleeab@gmail.com>
2019-02-22 15:07:16 +0100

    export table in tsv or csv

--- R/Writer.R
+++ R/Writer.R
@@ -1,1 +1,17 @@
 #' @importFrom data.table
+#' 
+#' export protein table to tsv or csv file 
+write_protein_table <- function(table, name = "protein_table", format = "tsv"){
+  
+  if (!format %in% c("tsv","csv")) {
+    stop("Please select a valid format to export the table. Options:  \"tsv\", \"csv\"")
+  }
+  
+  file <- paste(name, format, sep = ".")
+  if(format == "tsv"){
+    write.table(table, file=file, quote=FALSE, sep='\t', col.names = NA)
+  }else{
+    write.csv(table, file)
+  }
+}
+
