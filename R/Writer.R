39d9ac32066909cdc0cf2b0cd15ed5db2accd0b5
Chloe H. Lee <hjleeab@gmail.com>
2019-03-11 15:34:27 +0100

    write protein table manual

--- R/Writer.R
+++ R/Writer.R
@@ -1,20 +1,23 @@
-#' 
-#' export protein table to tsv or csv file 
+#' Export protein table to tsv or csv file 
 #'
-#' @param table 
-#' @param name 
-#' @param format 
-write_protein_table <- function(table, name = "protein_table", format = "tsv"){
+#' @param input_dt the object to be written, preferably data frame, data table or matrix
+#' @param file_name name of exported file 
+#' @param format format the object to be exported. Options include \code{"tsv"} and 
+#' \code{"csv"}. 
+#' @param ... other parameters of \code{write.table} function
+#'  
+#' @export
+write_protein_table <- function(input_dt, file_name = "protein_table", format = "tsv", ...){
   
   if (!format %in% c("tsv","csv")) {
     stop("Please select a valid format to export the table. Options:  \"tsv\", \"csv\"")
   }
   
-  file <- paste(name, format, sep = ".")
+  file <- paste(file_name, format, sep = ".")
   if(format == "tsv"){
-    write.table(table, file=file, quote=FALSE, sep='\t', col.names = NA)
+    write.table(input_dt, file=file, quote=FALSE, sep='\t', col.names = NA, ...)
   }else{
-    write.csv(table, file)
+    write.csv(input_dt, file, ...)
   }
 }
 
