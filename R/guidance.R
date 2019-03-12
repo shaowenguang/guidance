1acdde24a29ae64f89814a472569f56fb055aad2
Chloe H. Lee <hjleeab@gmail.com>
2019-03-12 16:23:50 +0100

    importFrom for guidance package only

--- R/guidance.R
+++ R/guidance.R
@@ -9,43 +9,17 @@
 #' To learn more about proBatch, start with the vignettes:
 #' \code{browseVignettes(package = "guidance")}
 #'
-#' @import dplyr
-#' @import ggfortify
-#' @import ggplot2
-#' @import pheatmap
-#' @import reshape2
-#' @import tibble
-#' @import lazyeval
-#' @import WGCNA
-#' @import viridis
-#' @import RColorBrewer
-#' @import readr
-#' @importFrom corrplot corrplot.mixed
-#' @importFrom magrittr %>%
-#' @importFrom purrr map
-#' @importFrom rlang UQ sym syms
-#' @importFrom tidyr complete nest unnest
-#' @importFrom data.table setDT
-#' @importFrom data.table transpose
-#' @importFrom data.table :=
-#' @importFrom data.table IDateTime
-#' @importFrom purrr map2
-#' 
-#' @docType package
-#' @name proBatch
-if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", 
-                                                        "batch_size", "batch_the_same", "batch_total", "category", "dateTime", 
-                                                        "fit", "label", "mean_fit", "median_batch", "median_global", 
-                                                        "median_run", "optimise_bw", "optimise_df", "peptide_col_name", 
-                                                        "sample_annotatation_col", "Step", "tipping.poings", "Var1", "Var2"))
-NULL
-
-
 #' @importFrom data.table, as.data.table
 #' @importFrom data.table, fread
 #' @importFrom data.table, dcast
 #' @importFrom data.table, copy
-
 #' @importFrom MASS lda
+#' 
+#' @docType package
+#' @name proBatch
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("input_dt"))
+NULL
+
+
 
 
