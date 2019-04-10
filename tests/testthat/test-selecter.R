7b66020de92b84320233003fdc207c0e60a27391
Chloe H. Lee <hjleeab@gmail.com>
2019-04-10 16:02:32 +0200

    test selector

--- tests/testthat/test-selecter.R
+++ tests/testthat/test-selecter.R
@@ -1,27 +1,76 @@
 context("selecter")
 
-peptideIons <- import_openswath(search_results= "data/QGS_SWATH_data", 
-                                sample_annotation="data/QGS_sample_annotation", 
-                                level="PeptideIon")
-all_peptideIons <- long2wide(peptideIons)
-peptideIon_n <- normalize_data(all_peptideIons, replaceNA="keep", normalization="none")
-d <- merge_replicates(peptideIon_n, anno)
-d_feature <- calc_features(d)
-d_feature_select <- perform_selection(d_feature)
-
 
 test_that("calc_features", {
-  peptideIons_features <- calc_features(all_peptideIons)
+  d_feature <- calc_features(peptideIon_st)
   
-  colnames <- colnames(peptideIons_features)
-  expect_equivalent(colnames[1:2], c("PeptideIon", "ProteinName" ))
-  expect_equivalent(colnames[63:65],  c("feature_mean_intensity_all",
-                    "feature_cv_intensity_all", "feature_numNA_intensity_all"))
+  expect_equivalent(colnames(d_feature)[1:2], c("PeptideIon", "ProteinName" ))
+  expect_equivalent(colnames(d_feature)[79:94], 
+                    c( "feature_mean_intensity_all",  "feature_cv_intensity_all", 
+                       "feature_numNA_intensity_all", "feature_averaged_score_all",
+                       "feature_sd_width_all",        "feature_median_PCC" , 
+                       "feature_median_SCC",          "feature_MAD_dist" , 
+                       "median_PCC_PerProt", "scaled_mean_intensity_all",
+                       "scaled_cv_intensity_all", "scaled_numNA_intensity_all", 
+                       "scaled_averaged_score_all", "scaled_sd_width_all",  
+                       "scaled_median_PCC","scaled_MAD_dist"))
   
-  expect_equivalent(round(mean(as.matrix(peptideIons_features$), na.rm=TRUE),2), 33409.88)
+  expect_equivalent(summary(as.matrix(d_feature$feature_mean_intensity_all))[1], "Min.   : 2383  ")
+  expect_equivalent(summary(as.matrix(d_feature$feature_mean_intensity_all))[2],  "1st Qu.: 6347  ")
+  expect_equivalent(summary(as.matrix(d_feature$feature_mean_intensity_all))[4], "Mean   :16927  ")
   
 })
 
-peptideIons_features <- calc_features(all_peptideIons)
-peptideIons_features_select <- perform_selection(peptideIons_features)
 
+test_that("perform_prediction_and_filtering", {
+  d_feature <- calc_features(peptideIon_st)
+  d_select <- perform_prediction_and_filtering(d_feature)
+
+  expect_equivalent(colnames(d_select)[1:2], c("PeptideIon", "ProteinName" ))
+  expect_equivalent(colnames(d_select)[79:95], 
+                    c( "feature_mean_intensity_all",  "feature_cv_intensity_all", 
+                       "feature_numNA_intensity_all", "feature_averaged_score_all",
+                       "feature_sd_width_all",        "feature_median_PCC" , 
+                       "feature_median_SCC",          "feature_MAD_dist" , 
+                       "median_PCC_PerProt", "scaled_mean_intensity_all",
+                       "scaled_cv_intensity_all", "scaled_numNA_intensity_all", 
+                       "scaled_averaged_score_all", "scaled_sd_width_all",  
+                       "scaled_median_PCC","scaled_MAD_dist", "prob"))
+  
+  expect_equivalent(summary(as.matrix(d_select$prob))[1], "Min.   :0.2026  ")
+  expect_equivalent(summary(as.matrix(d_select$prob))[2],  "1st Qu.:0.4008  ")
+  expect_equivalent(summary(as.matrix(d_select$prob))[4],  "Mean   :0.6021  ")
+  
+})
+
+
+test_that("get_lda_model", {
+  d_feature <- calc_features(peptideIon_st)
+  
+  ecoli_std <- c(2, 3, 4, 6, 8)
+  index_mean_int <- which(grepl("^mean_intensity", names(d_feature)))
+  d_feature[, cor_std := 0]
+  d_feature$cor_std <- apply(d_feature[, index_mean_int, with=F], 1, function(x) cor(x, ecoli_std, use="p"))
+  d_feature$cor_std[apply(d_feature[, index_mean_int, with=F], 1, function(x) count_pairwise_number(x, ecoli_std)) < 4] <- NA
+  d_feature[, label := "bad"]
+  d_feature[ cor_std > 0.95, ]$label <- "good"
+  
+  index_features <- c("scaled_mean_intensity_all", "scaled_cv_intensity_all", 
+                      "scaled_numNA_intensity_all", "scaled_averaged_score_all", 
+                      "scaled_median_PCC", "scaled_sd_width_all", "label")
+  model_lda <- get_lda_model(d_feature, index_features)
+  
+  expect_equivalent(names(model_lda$prior), c( "bad","good"))
+  expect_equivalent(round(model_lda$prior[1], 2), 0.56)
+  expect_equivalent(round(model_lda$prior[2],2), 0.44)
+  
+  expect_equivalent(dimnames(model_lda$means)[[2]], 
+                    c("scaled_mean_intensity_all", "scaled_cv_intensity_all",
+                      "scaled_numNA_intensity_all", "scaled_averaged_score_all", 
+                      "scaled_median_PCC", "scaled_sd_width_all")) 
+  
+  expect_equivalent(round(model_lda$means[1],2), -0.5)
+  expect_equivalent(round(model_lda$scaling[2], 2), -0.08)
+  expect_equivalent(round(model_lda$scaling[3], 2), -0.03)
+ 
+})
